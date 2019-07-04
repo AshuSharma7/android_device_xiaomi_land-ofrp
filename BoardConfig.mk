aba4ac5cceeb6b199340dfeb4d4d1c05bdae395c
Ashu Sharma <ashu7073657271@gmail.com>
2019-07-04 20:52:20 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,7 +83,7 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
 # Encryption
 TW_INCLUDE_CRYPTO := true
 TARGET_HW_DISK_ENCRYPTION := true
-TARGET_CRYPTFS_HW_PATH := $(DEVICE_PATH)/cryptfs_hw
+TARGET_CRYPTFS_HW_PATH := device/qcom/common/cryptfs_hw
 
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
